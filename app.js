4d3851f247a58a4fdb1643d71373f7a89f5a0169
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-06-08 03:17:34 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import * as THREE from '/three.module.js';
-import { OrbitControls } from '/OrbitControls.js';
+import * as THREE from '/libs/three/three.module.js';
+import { OrbitControls } from '/libs/three/jsm/OrbitControls.js';
 
 class App {
     constructor() {
